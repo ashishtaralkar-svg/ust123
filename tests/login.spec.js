33c018e4f93b13d55f4f3418b9fcfa02d299577b
ashish <ashishunico@gmail.com>
2026-03-11 11:58:08 +0530

    1990

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -9,7 +9,7 @@ test(`login${para.username}`, async({page})=>{
     await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
     //await page.pause();
     const Login= new login(page)
-    test.slow() //new //1//ashish
+    test.slow() //new //1//ashish //1990
     await Login.navigation(para.username,para.password)
     
     
